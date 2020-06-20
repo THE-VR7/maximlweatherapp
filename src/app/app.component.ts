02c2df90215f5e23548a88b6e3b985b2fb69655d
THE-VR7 <vineetrathor57@gmail.com>
2020-06-20 12:23:00 +0530

    Basic structure made,Responsive done,10% working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  public showedit=false
-  displayedit()
-  {
-    this.showedit=true;
+export class AppComponent {
+  public panels = [
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"", },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"", },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"", },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"", },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"", },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"", },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"", },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"", },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"", },
+  ]
+  displayedit(index:any){
+    this.panels[index].showsearch = true;
+    this.panels[index].hideclickme = false;
+    this.panels[index].showcity=false;
   }
+  showresults(index:any){
+    this.panels[index].showcity=true;
+    this.panels[index].showsearch=false;
+  }
+
+
 }
