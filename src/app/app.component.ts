b119315a7897a08f2e334002e1bdac8d75cec485
THE-VR7 <vineetrathor57@gmail.com>
2020-06-21 19:39:12 +0530

    working in visual studio code but niot in stackblitz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import {wetherService} from './weather.service';
+import {wetherService} from '../app/weather.service';
+import { AsyncValidatorFn, AbstractControl,ReactiveFormsModule, ValidationErrors, FormGroup, Validators, FormBuilder, FormArray, FormControl } from '@angular/forms';
+import { Observable, timer } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,26 +10,40 @@ import {wetherService} from './weather.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  constructor(private weatherservice : wetherService){}
-  
-  public rainurl = "https://sharerudition.com/wp-content/uploads/2018/12/Rain.jpg";
+  public checkcity = this.fb.array([ ]);
+  constructor(private weatherservice : wetherService,private fb : FormBuilder){}
+   public data:any;
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    for(var i=0;i<9;i++)
+    {
+          const group = this.fb.group({
+            'inputcity': new FormControl(null,[Validators.required,],[this.uniqueval()])
+          });
+          // console.log(group);
+        this.checkcity.push(group);
+    }
+    // console.log(this.checkcity.at(1));
+    // console.log(typeof(this.checkcity.at(1)));  
+  }
 
   public panels = [
-    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0, },
-    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0, },
-    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0, },
-    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0, },
-    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0, },
-    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0, },
-    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0, },
-    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0, },
-    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0, },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0,url:"" },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0,url:"" },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0,url:"" },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0,url:"" },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0,url:"" },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0,url:"" },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0,url:"" },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0,url:"" },
+    {showsearch:false,hideclickme:true,showcity:false,cityname:"",weather:"",temp:0,url:"" },
   ]
   displayedit(index:any){
     this.panels[index].showsearch = true;
     this.panels[index].hideclickme = false;
     this.panels[index].showcity=false;
+    this.checkcity.at(index).reset();
   }
   showresults(index:any,city:string){
     this.panels[index].showcity=true;
@@ -36,22 +53,47 @@ export class AppComponent {
 
   searchwaetherdata(cityname:string,index:any)
   {
-    this.weatherservice.searchweatherData(cityname).subscribe(
-      data =>{
+    // this.weatherservice.searchweatherData(cityname).subscribe(
+      // data =>{
         // console.log("data in the app.component");
-        console.log(data)
-        if(data.length!=0)
+        // console.log(this.data)
+        if(this.data.length!=0)
         {
-        this.panels[index].weather = data.weather[0].main;
-        this.panels[index].temp = data.main.temp;
-        }
-        else
-        {
-          
+        this.panels[index].weather = this.data.weather[0].main;
+        this.panels[index].temp = this.data.main.temp;
+        this.panels[index].cityname = this.data.name;
+        this.panels[index].url = "https://source.unsplash.com/1600x900/?weather,"+this.data.weather[0].main;
+        // console.log(this.panels[index].url)
+        
         }
 
-      }
-    )
+      
+    // )
+  }
+
+  uniqueval(): AsyncValidatorFn {
+    return (control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> => {
+      let debounceTime = 1000; //milliseconds
+      // console.log(control)
+      return timer(debounceTime).pipe(switchMap(()=> {
+      return this.weatherservice.searchweatherData(control.value).pipe(
+        map(data => {
+          if(data && data.length==undefined)
+          {
+            // console.log(data.length)
+            this.data = data;
+          }
+          else
+          {
+            // console.log("error executed")
+            // console.log(data.length)
+           return {"cityinvalid": true}
+          }
+        })
+      );
+    })
+  );
+  }
   }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import {wetherService} from './weather.service';
@@ -14,6 +14,8 @@ import {wetherService} from './weather.service';
     BrowserModule,
     FormsModule,
     HttpClientModule,
+        ReactiveFormsModule,
+
   ],
   providers: [wetherService],
   bootstrap: [AppComponent,]
